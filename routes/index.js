1e8241822f64e4e1fe3cd3940d86b15465f81124
willrsvpwdc <105689376+willrsvpwdc@users.noreply.github.com>
2022-05-17 06:48:20 +0000

    basic page routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res, next) {
+  res.sendFile('/public/home.html', { root: __dirname+"/.." });
+});
+
+router.get('/calendar', function(req, res, next) {
+  res.sendFile('/public/calendar.html', { root: __dirname+"/.." });
+});
+
+router.get('/events', function(req, res, next) {
+  res.sendFile('/public/events.html', { root: __dirname+"/.." });
+});
+
+router.get('/friends', function(req, res, next) {
+  res.sendFile('/public/friends.html', { root: __dirname+"/.." });
+});
+
+router.get('/notifications', function(req, res, next) {
+  res.sendFile('/public/notifications.html', { root: __dirname+"/.." });
 });
 
 module.exports = router;
