feb9275edf4261ffadb07cbe3a731636946689ef
NMazzy <84065477+NMazzy@users.noreply.github.com>
2022-05-19 08:13:35 +0000

    login and session test route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,45 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/test', function(req, res, next) {
+  if('session' in req)
+  {
+    res.json(req.session);
+  }
+  else
+  {
+    res.send('This is a test');
+  }
+});
+
+let users = {
+  admin1: { username: "admin1", name: "Some Admin", password: "admin" },
+  user1: { username: "user1", name: "Some User", password: "user" }
+};
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
+  if ('username' in req.body && 'password' in req.body)
+  {
+    if(req.body.username in users && users[req.body.username].password === req.body.password)
+    {
+      console.log('Sucess!');
+      req.session.user = users[req.body.username].username;
+      res.sendStatus(200);
+    }
+    else
+    {
+      console.log('Incorrect Username/Password');
+      res.sendStatus(401);
+    }
+  }
+  else
+  {
+    console.log('Inproper Login Form');
+    res.sendStatus(400);
+  }
+
+});
+
 router.get('/', function(req, res, next) {
   res.redirect('/home');
 });
