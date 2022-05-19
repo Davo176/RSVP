d40370ee2b2fcd506864d1fb9554db3d5d2b1d77
willrsvpwdc <105689376+willrsvpwdc@users.noreply.github.com>
2022-05-19 02:49:42 +0000

    Added Home Page

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,16 +1,13 @@
 function homeButton () {
-  alert("You pressed the home button");
-  location.href = "home.html"
+  location.href = "home"
 }
 
 function friendsButton () {
-  alert("You pressed the friends button");
-  location.href = "friends.html"
+  location.href = "friends"
 }
 
 function eventsButton () {
-  alert("You pressed the events button");
-  location.href = "myEvents.html"
+  location.href = "events"
 }
 
 function signUpButton () {
@@ -19,32 +16,6 @@ function signUpButton () {
 
 function loginButton () {
   alert("You pressed the login button");
-}
-
-function calendarButton () {
-  alert("You pressed the calendar button");
-  location.href = "calendar.html"
-}
-
-function notificationButton () {
-  alert("You pressed the notification button");
-  location.href = "notifications.html"
-}
-
-function createPostButton () {
-  alert("You pressed the create post button");
-}
-
-function acceptInvite () {
-  alert("You accepted your invite");
-}
-
-function maybeInvite () {
-  alert("You set your invite status to maybe");
-}
-
-function declineInvite () {
-  alert("You declined your invite");
 }
 
 function updateNav () {
@@ -60,11 +31,11 @@ function updateNav () {
 </nav>
 <nav>
   <ul class="navBar">
-      <li style="padding-right: 0;"><input class="search" type="text" v-model="search_text" placeholder="Search.."></li>
-      <li style="padding-left: 0;"><i href="#" class="icon fa-solid fa-magnifying-glass" v-on:click="search"></i></li>
-      <li><i href="#" class="icon fa-solid fa-plus" onclick="createPostButton()"></i></li>
-      <li><i href="#" class="icon fa-solid fa-bell" onclick="notificationButton()"></i></li>
-      <li><i href="#" class="icon fa-solid fa-calendar-days" onclick="calendarButton()"></i></li>
+      <li style="padding-right: 0;"><input class="search" type="text" placeholder="Search.."></li>
+      <li style="padding-left: 0;"><i href="#" class="icon fa-solid fa-magnifying-glass"></i></li>
+      <li><i href="#" class="icon fa-solid fa-plus"></i></li>
+      <li><i href="#" class="icon fa-solid fa-bell"></i></li>
+      <li><i href="#" class="icon fa-solid fa-calendar-days"></i></li>
       <li><button onclick="loginButton()">Log in</button></li>
       <li><button onclick="signUpButton()">Sign up</button></li>
   </ul>
