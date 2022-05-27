d608066b66143fc5fb0e48a2cf852ac144046266
willrsvpwdc <105689376+willrsvpwdc@users.noreply.github.com>
2022-05-27 13:50:22 +0000

    Created First Endpoint

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const moment = require('moment');
 const Uuid = require('uuid');
 
+const eventRouter = require('./events');
 const calendarRouter = require('./calendar');
 const friendRouter = require('./friends');
 const notificationRouter = require('./notifications');
@@ -19,6 +20,7 @@ router.get('/login', function(req, res, next) {
   res.send('server Healthy');
 });
 
+router.use('/events', eventRouter);
 router.use('/calendar', calendarRouter);
 router.use('/friends', friendRouter);
 router.use('/notifications', friendRouter);
