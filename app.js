0ae28d40ff0252c76bbe6ea1d4d3d7a73a5950dd
willrsvpwdc <105689376+willrsvpwdc@users.noreply.github.com>
2022-05-28 06:47:39 +0000

    Made it login

--- app.js
+++ app.js
@@ -32,6 +32,119 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.post('/login', function(req, res, next) {
+  if ('user_name' in req.body && 'password' in req.body)
+  {
+    req.pool.getConnection(function(error, connection)
+    {
+      if (error)
+      {
+        console.log(error);
+        res.sendStatus(500);
+        return;
+      }
+      console.log("Connected to database");
+      let user_name = req.body.user_name;
+      let password = req.body.password;
+      let query = "SELECT * FROM users WHERE user_name = ? and password_hash = ?";
+      connection.query(query,[user_name, password], function(error, rows, fields)
+      {
+        console.log("Got query");
+        connection.release();
+        if (error)
+        {
+          console.log(error);
+          res.sendStatus(500);
+          return;
+        }
+        if (rows.length == 0)
+        {
+          console.log('Incorrect Username/Password');
+          res.sendStatus(401);
+        }
+        if (rows.length == 1)
+        {
+          console.log('Logged In');
+          req.session.user_name = rows[0].user_name;
+          req.session.first_name = rows[0].first_name;
+          req.session.last_name = rows[0].last_name;
+          req.session.email = rows[0].email;
+          console.log(req.session);
+          res.sendStatus(200);
+        }
+      });
+
+    });
+  }
+  else
+  {
+    console.log('Inproper Login Form');
+    res.sendStatus(400);
+  }
+});
+
+app.post('/signup', function(req, res, next) {
+  console.log(req.body);
+
+  if ('user_name' in req.body && 'email' in req.body && 'password' in req.body && 'first_name' in req.body && 'last_name' in req.body)
+  {
+    req.pool.getConnection(function(error, connection)
+    {
+      if (error)
+      {
+        console.log(error);
+        res.sendStatus(500);
+        return;
+      }
+      console.log("Connected to database");
+      let user_name = req.body.user_name;
+      let email = req.body.email;
+      let password = req.body.password;
+      let first_name = req.body.first_name;
+      let last_name = req.body.last_name;
+      let check = "SELECT * FROM users WHERE user_name = ?";
+      connection.query(check,[user_name], function(error, rows, fields)
+      {
+        if (rows.length == 1)
+        {
+          console.log("User Exists");
+          res.sendStatus(403);
+          return;
+        }
+      });
+      let query = "INSERT INTO users (user_name, first_name, last_name, email, password_hash) VALUES (?,?,?,?,?)";
+      connection.query(query,[user_name, first_name, last_name, email, password], function(error, rows, fields)
+      {
+        connection.release();
+        console.log("Signed up");
+        req.session.user_name = user_name;
+        req.session.first_name = first_name;
+        req.session.last_name = last_name;
+        req.session.email = email;
+        console.log(req.session);
+        res.sendStatus(200);
+      });
+    });
+  }
+  else
+  {
+    console.log('bad request');
+    res.sendStatus(400);
+  }
+});
+
+app.get('/login', function(req, res, next) {
+  res.sendFile('/public/login.html', { root: __dirname });
+});
+
+app.use(function(req,res,next){
+  if (!("user_name" in req.session)){
+    res.redirect('/login');
+  }else{
+    next();
+  }
+})
+
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 app.use('/users', usersRouter);
