0fa7b28dff85ad689cfae4ee6b025dc92802d96b
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-06-09 07:57:36 +0000

    Thu, Jun 9, 2022, 7:57 AM +00:00

--- routes/forgottenPassword/index.js
+++ routes/forgottenPassword/index.js
@@ -11,9 +11,23 @@ router.get('/code', function(req, res, next){
             return;
         }
 
-        let query = "INSERT INTO users (forgotten_password_code)
+        let query = "UPDATE users SET forgotten_password_code = (SELECT RAND()*(1000000-100000)+100000) WHERE user_id=?";
+        connection.query(query, [req.session.user_id], function(error, rows, fields){
+            connection.release();
+            if(error) {
+                console.log(error);
+                res.sendStatus(500);
+                return;
+            }
+            res.sendStatus(200);
+        })
     })
 
 });
+
+router.post('/newPassword', function(req, res, next){
+
+    //verifies the code
+})
 
 module.exports = router;
