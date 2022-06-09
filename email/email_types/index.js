95f8c700007c20ddee51ae5a9bb2d3f33e9a8754
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-06-09 12:10:38 +0000

    Thu, Jun 9, 2022, 12:10 PM +00:00

--- email/email_types/index.js
+++ email/email_types/index.js
@@ -1,9 +1,11 @@
 const Finalise = require('./finalise.js')
 const Cancel = require('./cancel.js')
 const Response = require('./response.js')
+const forgottenPassword = require('./forgottenPassword.js');
 //compile all email types and export.
 module.exports = {
     Finalise,
     Cancel,
     Response,
+    forgottenPassword,
 }
