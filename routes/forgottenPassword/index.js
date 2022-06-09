eedc45d4c368b1ce767a149cfaf70d1e51853ca1
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-06-09 07:19:18 +0000

    Thu, Jun 9, 2022, 7:19 AM +00:00

--- routes/forgottenPassword/index.js
+++ routes/forgottenPassword/index.js
@@ -4,7 +4,15 @@ var router = express.router();
 //This function generates a 6 digit code, stores it in the database in the user's information and sends an email to the user containing the code
 router.get('/code', function(req, res, next){
 
-    req.pool.getConnection()
+    req.pool.getConnection(function (error, connection) {
+        if(error) {
+            console.log(error);
+            res.sendStatus(500);
+            return;
+        }
+
+        let query = "INSERT INTO users (forgotten_password_code)
+    })
 
 });
 
