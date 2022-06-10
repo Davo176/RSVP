39bfc17a683195497825cf3a62dc7c2d4109ba51
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-06-10 03:20:38 +0000

    Fri, Jun 10, 2022, 3:20 AM +00:00

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -98,49 +98,52 @@ function addAlertDiv() {
 
 function updateNav () {
   if(document.body.contains(document.querySelector("header"))){
-    var nav = document.getElementById('Nav');
-  if (first_name == "GUEST")
-  {
-    nav.innerHTML += `
-  <nav>
-    <ul class="navBar">
-        <li class="navBarElementContainerText">RSVP</li>
-    </ul>
-  </nav>
-  <nav>
-    <ul class="navBar">
-        <li class="navBarElementContainer"><button onclick="loginButton()">Sign Up</button></li>
-    </ul>
-  </nav>`;
-  }
-  else
-  {
-    nav.innerHTML += `
-  <nav>
-    <ul class="navBar">
-        <li class="navBarElementContainerText">RSVP</li>
-        <li class="navBarElementContainerText"><a href="/events" class="navBarElement">Home</a></li>
-        <li class="navBarElementContainerText"><a href="/friends" class="navBarElement">Friends</a></li>
+    document.querySelector("header").innerHTML += `<div id="loading"></div>`;
+    if(document.body.contains.getElementById("Nav")){
 
-    </ul>
-  </nav>
-  <nav>
-    <ul class="navBar">
-        <li class="navBarElementContainer"><a href="/newevent" title="Create new event"><i href="#" class="icon fa-solid fa-plus"></i></a></li>
-        <li class="navBarElementContainer"><a href="/calendar" title="Go to calendar"><i class="icon fa-solid fa-calendar-days"></i></a></li>
-        <li class="navBarElementContainerText" style="padding-right: 5px"><p id="user_name_placeholder"></p></li>
-        <li class="navBarElementContainer dropdown" style="padding-left: 0px"><i onclick="dropdown()" class="pointer fa-solid fa-circle-user dropdown"></i></a></li>
-    </ul>
-  </nav>
-  <ul style="list-style-type: none" id="dropdownContent" class="dropdown-content dropdown">
-        <li><a href="/account" class="dropdown">Edit account</a></li>
-        <li><a href="/logout" class="dropdown">Log out</a></li>
-  </ul>`;
-  let placeholder = document.getElementById("user_name_placeholder");
-  placeholder.innerText = `${first_name}`;
-  };
+      var nav = document.getElementById('Nav');
 
-  document.querySelector("header").innerHTML += `<div id="loading"></div>`;
+      if (first_name == "GUEST")
+      {
+        nav.innerHTML += `
+      <nav>
+        <ul class="navBar">
+            <li class="navBarElementContainerText">RSVP</li>
+        </ul>
+      </nav>
+      <nav>
+        <ul class="navBar">
+            <li class="navBarElementContainer"><button onclick="loginButton()">Sign Up</button></li>
+        </ul>
+      </nav>`;
+      }
+      else
+      {
+        nav.innerHTML += `
+      <nav>
+        <ul class="navBar">
+            <li class="navBarElementContainerText">RSVP</li>
+            <li class="navBarElementContainerText"><a href="/events" class="navBarElement">Home</a></li>
+            <li class="navBarElementContainerText"><a href="/friends" class="navBarElement">Friends</a></li>
+
+        </ul>
+      </nav>
+      <nav>
+        <ul class="navBar">
+            <li class="navBarElementContainer"><a href="/newevent" title="Create new event"><i href="#" class="icon fa-solid fa-plus"></i></a></li>
+            <li class="navBarElementContainer"><a href="/calendar" title="Go to calendar"><i class="icon fa-solid fa-calendar-days"></i></a></li>
+            <li class="navBarElementContainerText" style="padding-right: 5px"><p id="user_name_placeholder"></p></li>
+            <li class="navBarElementContainer dropdown" style="padding-left: 0px"><i onclick="dropdown()" class="pointer fa-solid fa-circle-user dropdown"></i></a></li>
+        </ul>
+      </nav>
+      <ul style="list-style-type: none" id="dropdownContent" class="dropdown-content dropdown">
+            <li><a href="/account" class="dropdown">Edit account</a></li>
+            <li><a href="/logout" class="dropdown">Log out</a></li>
+      </ul>`;
+      let placeholder = document.getElementById("user_name_placeholder");
+      placeholder.innerText = `${first_name}`;
+      };
+    }
   }
 
 }
